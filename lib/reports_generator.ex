3a20c6191dd717209c11835ac74869116e426782
Felipe Augusto Rosa <flp.far@gmail.com>
2021-08-25 06:45:48 -0300

    Add file parser

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,5 +1,11 @@
 defmodule ReportsGenerator do
-  def hello do
-    :world
+  def build(filename) do
+    filename
+    |> ReportsGenerator.Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
+
+  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
